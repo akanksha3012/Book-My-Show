ba37aaa19622f47514bf283b8cf379edd7f950f2
akanksha3012 <burnwal.akanksha30@gmail.com>
2021-09-10 02:43:05 +0530

    Tmdb integration

--- src/components/HeroCarousel/HeroCarousel.Component.jsx
+++ src/components/HeroCarousel/HeroCarousel.Component.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import HeroSlider from "react-slick";
+import axios from 'axios';
 
 //Arrow components
  import { NextArrow, PrevArrow} from "./Arrows.Component";
 
 const HeroCarousel = () => {
-    const [images, setImages] = useState([
-        "https://in.bmscdn.com/promotions/cms/creatives/1630474590730_mardkodardnhihota_1240x300_1sep.jpg",
-        "https://in.bmscdn.com/promotions/cms/creatives/1629830049996_celebfiecruisewiththestarsrevised_webshowcase_1240x300_24aug.jpg",
-        "https://in.bmscdn.com/promotions/cms/creatives/1628591224466_fnbgeneric.jpg",
-        "https://in.bmscdn.com/promotions/cms/creatives/1630608713679_matrixfightnight6_webshowcase_1240x300.jpg",
-        "https://in.bmscdn.com/promotions/cms/creatives/1630767397989_incinemas10thseptemberknowmore_webshowcase_1240x300.jpg",
-    ]);
+    const [images, setImages] = useState([]);
+    useEffect(() => {
+        const requestNowPlayingMovies = async () => {
+            const getImages = await axios.get('/movie/now_playing');
+            setImages(getImages.data.results)
+        }
+        requestNowPlayingMovies();
+    },[])
 
     const settingsLG = {
         arrows: true,
         autoplay: true,
         centerMode: true,
-        centerPadding: "20px",
+        centerPadding: "200px",
         slidesToShow: 1,
         infinite: true,
         slideToScroll: 1,
@@ -42,9 +44,9 @@ const HeroCarousel = () => {
                     {images.map((image) => (
                         <div className="w-full h-56 md:h-80 py-3">
                             <img
-                                src={image}
+                                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                                 alt="Hero Banner"
-                                className="w-full h-full rounded-md object-center"
+                                className="w-full h-full rounded-md object-center "
                             />
                         </div>
                     ))}
@@ -55,9 +57,9 @@ const HeroCarousel = () => {
                     {images.map((image) => (
                         <div className="w-full h-96 px-2 py-3">
                             <img
-                                src={image}
+                                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                                 alt="Hero Banner"
-                                className="w-full h-full rounded-md object-center"
+                                className="w-full h-full rounded-md object-center "
                             />
                         </div>
                     ))}
--- src/pages/Home.Page.jsx
+++ src/pages/Home.Page.jsx
@@ -1,106 +1,36 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
 import EntertainmentCardSlider from '../components/Entertainment/EntertainmentCard.Component';
 import HeroCarousel from '../components/HeroCarousel/HeroCarousel.Component';
 import PosterSlider from '../components/PosterSlider/PosterSlider.Component';
+import axios from 'axios';
 
 function HomePage() {
-    const recommendedMovies = [
-       { 
-           src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzklICA5ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00056556-cmljhpnhcd-portrait.jpg",
-           title:"Fast and Furious 9",
-           subtitle:"Action / Adventure / Crime / Thriller",
-       },
-       { 
-           src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzklICA5ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00056556-cmljhpnhcd-portrait.jpg",
-           title:"Fast and Furious 9",
-           subtitle:"Action / Adventure / Crime / Thriller",
-       },
-       { 
-           src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzklICA5ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00056556-cmljhpnhcd-portrait.jpg",
-           title:"Fast and Furious 9",
-           subtitle:"Action / Adventure / Crime / Thriller",
-       },
-       { 
-           src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzklICA5ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00056556-cmljhpnhcd-portrait.jpg",
-           title:"Fast and Furious 9",
-           subtitle:"Action / Adventure / Crime / Thriller",
-       },
-       { 
-           src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzklICA5ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00056556-cmljhpnhcd-portrait.jpg",
-           title:"Fast and Furious 9",
-           subtitle:"Action / Adventure / Crime / Thriller",
-       },
-       { 
-           src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzklICA5ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00056556-cmljhpnhcd-portrait.jpg",
-           title:"Fast and Furious 9",
-           subtitle:"Action / Adventure / Crime / Thriller",
-       },
-       
-    ];
-    const premierMovies = [
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-OTElICAxOWsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122566-rhfsjjrlrc-portrait.jpg",
-            title:"Sang-Chi and the legend of ten rings",
-            subtitle:"English",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-OTElICAxOWsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122566-rhfsjjrlrc-portrait.jpg",
-            title:"Sang-Chi and the legend of ten rings",
-            subtitle:"English",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-OTElICAxOWsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122566-rhfsjjrlrc-portrait.jpg",
-            title:"Sang-Chi and the legend of ten rings",
-            subtitle:"English",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-OTElICAxOWsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122566-rhfsjjrlrc-portrait.jpg",
-            title:"Sang-Chi and the legend of ten rings",
-            subtitle:"English",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-OTElICAxOWsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122566-rhfsjjrlrc-portrait.jpg",
-            title:"Sang-Chi and the legend of ten rings",
-            subtitle:"English",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-OTElICAxOWsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122566-rhfsjjrlrc-portrait.jpg",
-            title:"Sang-Chi and the legend of ten rings",
-            subtitle:"English",
-        },
-    ]
-    const onlineStreamEvents = [
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-V2VkLCA4IFNlcCBvbndhcmRz,ots-29,otc-FFFFFF,oy-612,ox-24/et00313611-xqqvejluju-portrait.jpg",
-            title:"FrontRow open-mic",
-            subtitle:"Watch on Zoom",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-V2VkLCA4IFNlcCBvbndhcmRz,ots-29,otc-FFFFFF,oy-612,ox-24/et00313611-xqqvejluju-portrait.jpg",
-            title:"FrontRow open-mic",
-            subtitle:"Watch on Zoom",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-V2VkLCA4IFNlcCBvbndhcmRz,ots-29,otc-FFFFFF,oy-612,ox-24/et00313611-xqqvejluju-portrait.jpg",
-            title:"FrontRow open-mic",
-            subtitle:"Watch on Zoom",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-V2VkLCA4IFNlcCBvbndhcmRz,ots-29,otc-FFFFFF,oy-612,ox-24/et00313611-xqqvejluju-portrait.jpg",
-            title:"FrontRow open-mic",
-            subtitle:"Watch on Zoom",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-V2VkLCA4IFNlcCBvbndhcmRz,ots-29,otc-FFFFFF,oy-612,ox-24/et00313611-xqqvejluju-portrait.jpg",
-            title:"FrontRow open-mic",
-            subtitle:"Watch on Zoom",
-        },
-        { 
-            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-V2VkLCA4IFNlcCBvbndhcmRz,ots-29,otc-FFFFFF,oy-612,ox-24/et00313611-xqqvejluju-portrait.jpg",
-            title:"FrontRow open-mic",
-            subtitle:"Watch on Zoom",
-        },
-    ]
+    const [recommendedMovies, setRecommendedMovies] = useState([]);
+    const [premierMovies, setPremierMovies] = useState([]);
+    const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
+
+    useEffect(() => {
+        const requestPopularMovies = async () => {
+            const getPopularMovies = await axios.get('/movie/popular');
+            setRecommendedMovies(getPopularMovies.data.results)
+        }
+        requestPopularMovies();
+    },[])
+    useEffect(() => {
+        const requestTopRatedMovies = async () => {
+            const getTopRatedMovies = await axios.get('/movie/top_rated');
+            setPremierMovies(getTopRatedMovies.data.results)
+        }
+        requestTopRatedMovies();
+    },[])
+    useEffect(() => {
+        const requestUpcomingMovies = async () => {
+            const getUpcomingMovies = await axios.get('/movie/upcoming');
+            setOnlineStreamEvents(getUpcomingMovies.data.results)
+        }
+        requestUpcomingMovies();
+    },[])
+
     return (
         <>
             <HeroCarousel />
